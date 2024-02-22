b27808cdccd22f97595f72a28f118429d54fdc6a
ranjith4826 <ranjithrock787@gmail.com>
2024-02-22 10:52:32 +0530

    code pushing

---
+++ src/codepushtogit/Ranjithclass.java
@@ -0,0 +1,12 @@
+package codepushtogit;
+
+public class Ranjithclass
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Done");
+
+	}
+
+}
---
+++ src/codepushtogit/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author PC
+ *
+ */
+package codepushtogit;
